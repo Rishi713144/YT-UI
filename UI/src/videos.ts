b947f946958219e055fa44940e5332b68eb2dcc7
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-11-13 18:48:48 +0530

    initial commit

---
+++ UI/src/videos.ts
@@ -0,0 +1,80 @@
+export interface Video {
+    title: string;
+    thumbnail: string;
+    description: string;
+    viewCount: string;
+    timestamp: string;
+}
+
+export const VIDEOS: Video[] = [
+    {
+        title: "Avengers: Endgame - Final Battle Scene (4K)",
+        thumbnail: "https://i.pinimg.com/1200x/31/53/f0/3153f0cfcac15fdcb76bf089136ebffb.jpg",
+        description: "Marvel India Official",
+        viewCount: "8.9M",
+        timestamp: "2 days ago"
+    },
+    {
+        title: "Thor vs Hulk - Ragnarok Arena Fight | Hindi",
+        thumbnail: "https://i.pinimg.com/1200x/13/eb/7d/13eb7d6fc1af82c91552db975667fc9d.jpg",
+        description: "Marvel Entertainment Hindi",
+        viewCount: "5.2M",
+        timestamp: "1 week ago"
+    },
+    {
+        title: "Iron Man Suit Up Scene Compilation (2008-2019)",
+        thumbnail: "https://i.pinimg.com/1200x/92/04/a4/9204a4fc0c9445920ab8ed84a839f508.jpg",
+        description: "Marvel",
+        viewCount: "12.4M",
+        timestamp: "3 weeks ago"
+    },
+    {
+        title: "Spider-Man No Way Home - All 3 Spideys Meet | Hindi Dub",
+        thumbnail: "https://i.pinimg.com/1200x/1e/22/ea/1e22ea2564bac43f8875b5c78a51768b.jpg",
+        description: "Sony Pictures India",
+        viewCount: "15.8M",
+        timestamp: "4 days ago"
+    },
+    {
+        title: "Captain America Lifts Mjolnir - Avengers Endgame | Hindi",
+        thumbnail: "https://i.pinimg.com/736x/e3/d2/09/e3d209b5190e677c79005f8037a3b1ee.jpg",
+        description: "Marvel HQ India",
+        viewCount: "22.1M",
+        timestamp: "1 month ago"
+    },
+    {
+        title: "Deadpool & Wolverine - Official Trailer 2 (Hindi)",
+        thumbnail: "https://i.pinimg.com/1200x/b1/5a/27/b15a2703a26218d5e3363366e25ddd38.jpg",
+        description: "Marvel India",
+        viewCount: "9.7M",
+        timestamp: "12 hours ago"
+    },
+    {
+        title: "Thanos Snap Scene - Infinity War | Hindi 4K",
+        thumbnail: "https://i.pinimg.com/1200x/37/49/96/37499639e48f8a420020bd6747fb45de.jpg",
+        description: "Marvel Cinematic Universe Hindi",
+        viewCount: "18.5M",
+        timestamp: "5 days ago"
+    },
+    {
+        title: "Loki Season 2 Finale Explained in Hindi",
+        thumbnail: "https://i.pinimg.com/736x/cd/20/e0/cd20e0cf4651eec53342924e6d205424.jpg",
+        description: "Marvel Adda",
+        viewCount: "3.1M",
+        timestamp: "6 hours ago"
+    },
+    {
+        title: "Black Panther Wakanda Forever - Namor Entry Scene",
+        thumbnail: "/avg.jpg",
+        description: " Marvel India",
+        viewCount: "7.3M",
+        timestamp: "2 weeks ago"
+    },
+    {
+        title: "Doctor Strange Multiverse of Madness - Illuminati Cameos",
+        thumbnail: "/avg.jpg",
+        description: "Big Nick Fury",
+        viewCount: "11.2M",
+        timestamp: "1 day ago"
+    }
+]
